ef5bc224774f28fe91ac57a85a4348dd0bb16f01
gutonin <gutonin@gmail.com>
2025-04-21 22:10:26 -0300

    [Update][FIX][ADD] Views, template
    Add a view for update tasks, fix creation tasks on query, update template to add update form.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = SQLAlchemy(app)
 
 #https://www.youtube.com/watch?v=mWBNI1cS0jg
 
-#Definindo o model
+#Model Definition
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), unique=True, nullable=False)
@@ -20,7 +20,7 @@ def index():
 @app.route('/create', methods=['POST'])
 def create_task():
     form_description = request.form['description']
-    task_exists = Tasks.query.filter_by(descrption=form_description).first()
+    task_exists = Tasks.query.filter_by(description=form_description).first()
     if task_exists:
         return 'Error: Task already exists!', 400
     new_task = Tasks(description=form_description)
@@ -28,15 +28,24 @@ def create_task():
     db.session.commit()
     return redirect('/')
 
-@app.route('/delete<int:task_id>', methods=['POST'])
+@app.route('/update/<int:task_id>', methods=['POST'])
+def update_task(task_id):
+    task = Tasks.query.get(task_id)
+    if task:
+        task.description = request.form['description']
+        db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
     task = Tasks.query.get(task_id)
     if task:
         db.session.delete(task)
         db.session.commit()
-    redirect('/')
+    return redirect('/')
 
 if __name__ == '__main__':
+    #Create database structure
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=5050)
